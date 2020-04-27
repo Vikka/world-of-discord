cc0fb0cfbccc7b0bf2ed0fc5b80b372f985d9e5d
Dorian Turba <dorian.turba@gmail.com>
2020-04-27 14:15:36 +0200

    refactor: flag in leaderboard

--- src/commands/character.py
+++ src/commands/character.py
@@ -215,10 +215,8 @@ class Personnage(Cog):
                     path, characters = _get_path_and_characters(member, guild)
                     tmp_exp = 0
                     for character in characters.values():
-                        if character.total_exp > tmp_exp:
-                            tmp_exp = character.total_exp
-                    if tmp_exp > max_exp:
-                        max_exp = tmp_exp
+                        tmp_exp = max(character.total_exp, tmp_exp)
+                    max_exp = max(tmp_exp, max_exp)
                 guild_list.append((guild.name, max_exp))
             guild_list.sort(key=lambda x: x[1], reverse=True)
             embed = Embed(title='Classement des Guildes')
@@ -226,20 +224,20 @@ class Personnage(Cog):
             embed.add_field(name='Nom', value=guild_list[0][0])
             embed.add_field(name='Exp max', value=guild_list[0][1])
             names = 1
-            values = 1
+            complet = True
             for i, guild in enumerate(guild_list[1:], start=2):
                 if i % 2 == 0:
                     names = (str(i), guild[0], guild[1])
-                else:
-                    values = (str(i), guild[0], guild[1])
-                    embed.add_field(name=names[0], value=values[0])
-                    embed.add_field(name=names[1], value=values[1])
-                    embed.add_field(name=names[2], value=values[2])
-                    values = 0
-            if values != 0:
-                embed.add_field(name=str(len(guild_list)), value='\u200b')
-                embed.add_field(name=guild_list[-1][0], value='\u200b')
-                embed.add_field(name=guild_list[-1][1], value='\u200b')
+                    complet = False
+                    continue
+                embed.add_field(name=names[0], value=str(i))
+                embed.add_field(name=names[1], value=guild[0])
+                embed.add_field(name=names[2], value=guild[1])
+                complet = True
+            if not complet:
+                embed.add_field(name=names[0], value='\u200b')
+                embed.add_field(name=names[1], value='\u200b')
+                embed.add_field(name=names[2], value='\u200b')
             await context.send(embed=embed)
 
         ...
