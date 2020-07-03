d8b8f73c4e3a4e43ee27d464b741331e031ced47
Dorian Turba <dorian.turba@gmail.com>
2020-07-03 17:32:49 +0200

    feat: giveaway cmd

--- src/commands/admin.py
+++ src/commands/admin.py
@@ -1,9 +1,13 @@
+from asyncio import sleep
+from random import choice
+
 from discord import Guild, PermissionOverwrite
 from discord.ext.commands import Cog, command, Context, Bot
 from discord.utils import get
 
 from src.errors.guild import ChannelAlreadyExist
 from src.commands.utils import is_admin, is_owner
+from src.manipulation.leaderboard.local_leaderboard import get_max_xp
 
 
 class Admin(Cog):
@@ -32,6 +36,29 @@ class Admin(Cog):
                                         overwrites=event_permission)
         await guild.create_text_channel('commandes', category=cat)
 
+    @command(checks=[is_admin])
+    async def giveaway(self, context: Context, *, msg):
+        members = list()
+        for member in context.guild.members:
+            if (max_xp := get_max_xp(member)) > 0:
+                members.append((member.name, max_xp))
+        winner = choice(members)[0]
+        await context.send(f'La grande loterie va bientôt commencer !!!\n'
+                           f'Installez vous, prenez des sucreries, nous allons'
+                           f' bientôt tirer au sort le grand gagnant !')
+        await sleep(10)
+        await context.send(f'Le tirage au sort va être lancé dans 10...')
+        await sleep(1)
+        for i in range(9, 0, -1):
+            await context.send(f'{i}...')
+            await sleep(1)
+        await context.send(f'NOUS AVONS UN GAGNANT !!!!!!!!!')
+        await sleep(5)
+        await context.send(f'Le gagnant de la loterie est .....')
+        await sleep(2)
+        await context.send(f"{winner.capitalize()} !!!!!!!!!!!!")
+        await sleep(1)
+        await context.send(f'{winner} remporte donc {msg}!!!!')
 
 def setup(bot):
     bot.add_cog(Admin(bot))
