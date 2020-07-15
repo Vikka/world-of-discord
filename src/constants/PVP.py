e55a670697cd184c3f5466e865f0dcda5485b006
Dorian Turba <dorian.turba@gmail.com>
2020-07-15 16:18:45 +0200

    fix: #14

--- src/constants/PVP.py
+++ src/constants/PVP.py
@@ -1,6 +1,6 @@
 REGISTERED = '{} est entré en lice !'
 DM_REGISTERED = 'Enregistrement de ton personnage'
-INIT = "Le duel peux commencer. Votre habilité au combat vous donne " \
+INIT = "Le duel peut commencer. Votre habilité au combat vous donne " \
        "l'initiative !"
 
 ATTACK = 1
