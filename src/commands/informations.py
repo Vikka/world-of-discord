62697258fef4330c7b22278ab8e5c9e21b8b0d57
Dorian Turba <dorian.turba@gmail.com>
2020-07-17 14:23:29 +0200

    feat: issues command

--- src/commands/informations.py
+++ src/commands/informations.py
@@ -2,6 +2,7 @@ from discord.ext.commands import Bot, Cog, command, Context
 
 from src.commands.utils import no_direct_message, in_command_channel
 from src.constants.INFO import GITHUB_PROJECT
+from src.constants.INFORMATIONS import ISSUE_TEMPLATE_TEXT
 
 
 class Informations(Cog):
@@ -18,7 +19,17 @@ class Informations(Cog):
         """
         await context.channel.send(GITHUB_PROJECT)
 
+    @command(name='issues', aliases=['issue', 'i'],
+             checks=[in_command_channel])
+    async def issue(self, context: Context):
+        """
+        Lien pour demander des fonctionnalités ou remonter des bogues.
 
+        Les modèles d'issues sont traduits. Si vous souhaitez faire votre
+        commentaire en français, pensez à sélectionner une issue précédée par
+        les lettres "FR", les isntructions sont traduites pour vous aider.
+        """
+        await context.send(ISSUE_TEMPLATE_TEXT)
 
 def setup(bot):
     bot.add_cog(Informations(bot))
---
+++ src/constants/INFORMATIONS.py
@@ -0,0 +1,11 @@
+ISSUE_TEMPLATE_LINK = \
+    'https://github.com/Vikka/world-of-discord/issues/new/choose'
+ISSUE_TEMPLATE_TEXT = "Que tu souhaites remonter un problème, un bogue, " \
+                      "ou demander une nouvelle fonctionnalité, tu dois " \
+                      "suivre ce lien.\n" \
+                      "Il s'y trouve une liste de modèles " \
+                      "ayant pour objectif de t'aider dans la redaction " \
+                      f"de ta demande.\n\n" \
+                      f"⚠️ Pour pouvoir envoyer ta demande, tu dois avoir un " \
+                      f"compte utilisateur sur ce site.\n\n" \
+                      f"{ISSUE_TEMPLATE_LINK}"
