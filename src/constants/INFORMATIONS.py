c4df0e1b2c92ce4c69f6423635970170f40c7ac3
Dorian Turba <dorian.turba@gmail.com>
2020-07-20 10:14:37 +0200

    fix: bug and enhancement list link

--- src/constants/INFORMATIONS.py
+++ src/constants/INFORMATIONS.py
@@ -1,9 +1,11 @@
-BUG_LIST_LINK = 'https://github.com/Vikka/world-of-discord/labels/bug'
+BUG_LIST_LINK = 'https://github.com/Vikka/world-of-discord/issues?' \
+                'q=is%3Aissue+is%3Aopen+label%3Abug'
 BUG_LIST_TEXT = "Voici le lien qui te permet de consulter la liste des " \
                 "bogues connus. Si tu en constate un qui n'est pas dans la " \
                 "liste, n'hésite pas à le soumettre !\n\n" \
                 f"{BUG_LIST_LINK}"
-ENHANCEMENT_LIST_LINK = 'https://github.com/Vikka/world-of-discord/labels/bug'
+ENHANCEMENT_LIST_LINK = 'https://github.com/Vikka/world-of-discord/issues?' \
+                        'q=is%3Aissue+is%3Aopen+label%3Aenhancement'
 ENHANCEMENT_LIST_TEXT = "Voici le lien qui te permet de consulter la liste " \
                         "des améliorations proposées. Si tu as une idée qui " \
                         "mériterais de s'y trouver, n'hésite pas à la " \
